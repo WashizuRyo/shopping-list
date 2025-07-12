5524dd82cec32edb756268d93b0886667db5afc6
Ryo Washizu <wasizuryo@gmail.com>
2025-07-12 21:52:57 +0900

    紐づいているitemを表示

--- resources/js/pages/shopping-list/show.tsx
+++ resources/js/pages/shopping-list/show.tsx
@@ -11,7 +11,14 @@ export default function ShoppingList({ shoppingList }: { shoppingList: ShoppingL
             <h1>{shoppingList.name}</h1>
             <p>Created at: {shoppingList.created_at}</p>
             <p>Updated at: {shoppingList.updated_at}</p>
-
+            {shoppingList.items?.map((item) => (
+                <div key={item.id}>
+                    <h2>{item.name}</h2>
+                    <p>Memo: {item.memo}</p>
+                    <p>Quantity: {item.pivot?.quantity}</p>
+                    <p>Checked: {item.pivot?.is_checked ? 'Yes' : 'No'}</p>
+                </div>
+            ))}
         </div>
     );
 }

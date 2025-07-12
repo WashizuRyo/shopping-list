abe8443ffba83ddb83054e844ff9abb64d5a253d
Ryo Washizu <wasizuryo@gmail.com>
2025-07-12 21:12:39 +0900

    shoppingList, Itemを関連付け

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,23 +7,35 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class ShoppingList extends Model
+class Item extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'user_id',
         'name',
+        'memo',
     ];
 
+    /**
+     * Get the user that owns the item.
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function items(): BelongsToMany
+    /**
+     * The shopping lists that belong to the item.
+     */
+    public function shoppingLists(): BelongsToMany
     {
-        return $this->belongsToMany(Item::class, 'item_shopping_list')
+        return $this->belongsToMany(ShoppingList::class, 'item_shopping_list')
             ->withPivot('quantity', 'is_checked')
             ->withTimestamps();
     }

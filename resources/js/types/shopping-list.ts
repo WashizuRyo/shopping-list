e9d3a7c6a774677cd888e13314e267d98c023bd1
Ryo Washizu <wasizuryo@gmail.com>
2025-07-12 21:52:10 +0900

    Itemの型を定義

--- resources/js/types/shopping-list.ts
+++ resources/js/types/shopping-list.ts
@@ -1,7 +1,23 @@
+export type Item = {
+    id: number;
+    name: string;
+    memo?: string;
+    user_id: number;
+    created_at: string;
+    updated_at: string;
+    pivot?: {
+        quantity: number;
+        is_checked: boolean;
+        created_at: string;
+        updated_at: string;
+    };
+};
+
 export type ShoppingList = {
-  id: number;
-  name: string;
-  user_id: number;
-  created_at: string;
-  updated_at: string;
-}
+    id: number;
+    name: string;
+    user_id: number;
+    created_at: string;
+    updated_at: string;
+    items?: Item[];
+};

f8609d9801d9bb31634c363b4ae8438a8bca0d39
Ryo Washizu <wasizuryo@gmail.com>
2025-07-18 21:13:06 +0900

    コメント削除

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -11,28 +11,17 @@ class Item extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'user_id',
         'name',
         'memo',
     ];
 
-    /**
-     * Get the user that owns the item.
-     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    /**
-     * The shopping lists that belong to the item.
-     */
     public function shoppingLists(): BelongsToMany
     {
         return $this->belongsToMany(ShoppingList::class, 'item_shopping_list')
